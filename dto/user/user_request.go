fbfcc26bfb38063b68d5a35d3d9a2f29054c5848
Faeq <faequl96@gmail.com>
2023-01-20 23:41:59 +0700

    update request change

--- dto/user/user_request.go
+++ dto/user/user_request.go
@@ -1,10 +1,10 @@
 package userdto
 
 type UserUpdateRequest struct {
-	Name     string `json:"name" gorm:"type: varchar(255)" validate:"required"`
-	Email    string `json:"email" gorm:"type: varchar(255)" validate:"required"`
+	Name     string `json:"name" gorm:"type: varchar(255)"`
+	Email    string `json:"email" gorm:"type: varchar(255)"`
 	Password string `json:"password" gorm:"type: varchar(255)"`
-	Image    string `json:"image" gorm:"type: varchar(255)" validate:"required"`
+	Image    string `json:"image" gorm:"type: varchar(255)"`
 	Phone    string `json:"phone" gorm:"type: varchar(255)"`
 	PosCode  string `json:"pos_code" gorm:"type: varchar(255)"`
 	Address  string `json:"address" gorm:"type: varchar(255)"`
